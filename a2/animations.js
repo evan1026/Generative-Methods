ff3a781d093c350ab903fa4e70dfc9b07b0b5e0d
Evan Allan <evanallan1026@gmail.com>
2022-10-05 16:44:34 -0500

    Bouncy ball

--- a2/animations.js
+++ a2/animations.js
@@ -2,6 +2,61 @@ const SWATCH_SIZE = 300;
 
 let animations = [
   {
+    title: "Bouncy Ball",
+    description: "Just a bouncy ball.",
+    isActive: true,
+
+    setup(p) {
+      this.ballRadius = 20;
+      this.bounceHeight = 50;
+      this.bounceSpeed = 2;
+      this.minY = 10;
+      this.loopTime = 3;
+    },
+    draw(p, t) {
+      let percent = (t % this.loopTime) / this.loopTime;
+
+      p.noStroke();
+      p.background(100);
+
+      let x = percent * p.width;
+      let y = 2 * this.bounceHeight * Math.abs(Math.cos(this.bounceSpeed * percent * p.PI)) + this.minY;
+      let ballHeight = this.ballRadius;
+      let ballWidth = this.ballRadius;
+      let ballArea = ballHeight * ballWidth;
+
+      let bounce = false;
+      if (y < ballHeight) {
+        bounce = true;
+        ballHeight = y;
+        ballWidth = ballArea / ballHeight;
+      }
+
+      // Flip bc graphics do y starting at the top
+      y = p.height - y;
+
+      if (bounce) {
+        p.stroke(0);
+        p.line(x - 21, p.height - 3, x - 32, p.height - 7);
+        p.line(x - 20, p.height - 5, x - 30, p.height - 10);
+        p.line(x - 18, p.height - 6, x - 27, p.height - 12);
+        p.line(x + 21, p.height - 3, x + 32, p.height - 7);
+        p.line(x + 20, p.height - 5, x + 30, p.height - 10);
+        p.line(x + 18, p.height - 6, x + 27, p.height - 12);
+      }
+
+      p.noStroke();
+      p.fill(0, 75, 50);
+      p.ellipse(x, y, ballWidth, ballHeight);
+
+      if (x < this.ballRadius) {
+        p.ellipse(p.width + x, y, ballWidth, ballHeight);
+      } else if (x > p.width - this.ballRadius) {
+        p.ellipse(x - p.width, y, ballWidth, ballHeight);
+      }
+    },
+  },
+  {
     title: "Rainbow Spiral",
     description: "I was experimenting with using the previous frame in the creation of the new frame, and after a bunch of playing I got this. It creates a lot of cool patterns that don't seem to ever repeat exactly, kind of like a kaleidoscope. If you watch for long enough you will eventually see the spiral unravel itself and then wrap back around the other way.",
     isActive: true,
