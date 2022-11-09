fc9eda85777ef204f8b8c35d42a8ffb57d781de0
Evan Allan <evanallan1026@gmail.com>
2022-11-08 23:36:48 -0600

    Part 2 done

--- a5/generators.js
+++ a5/generators.js
@@ -44,4 +44,109 @@ const GENERATORS = {
       p.pop();
     },
   },
+
+  cats: {
+    description: "meow",
+
+    sliders: [
+      "Fur Saturation",
+      "Fur Lightness",
+      "Eye Color",
+      "Pupil",
+      "Whisker Color",
+    ],
+
+    setup(p, index) {
+      if (!this.blinkTimes) {
+        this.blinkTimes = [];
+      }
+
+      this.blinkTimes.push(Math.random() * 50 + 10);
+    },
+
+    landmarks: {
+      "Nutmeg": [0.40,0.66,0.22,0.54,1.00],
+      "Autumn": [0.38,0.12,0.32,0.32,0.40],
+      "Winter": [0.00,0.70,0.16,0.42,1.00],
+      "Pepper": [0.00,0.08,0.12,0.42,0.34],
+      "Sammy":  [0.72,0.64,0.28,0.18,1.00]
+    },
+
+    draw(p, t, dna, index) {
+      p.push();
+
+      let furSaturation = dna[0] * 100;
+      let furLightness = dna[1] * 100;
+      let eyeColor = dna[2] * 180 + 30;
+      let pupilWidth = dna[3] * 8.5 + 0.5;
+      let whiskerColor = dna[4] * 100;
+
+      // Ears
+      p.fill(20, furSaturation, furLightness);
+      p.strokeWeight(0.5);
+      p.stroke(0);
+      p.triangle(-45, -115, -25, -175, 0, -125);
+      p.triangle(45, -115, 25, -175, 0, -125);
+
+      // Inner ears
+      p.noStroke();
+      p.fill(20, furSaturation, furLightness * 0.9);
+      p.triangle(-35, -125, -24, -165, -10, -135);
+      p.triangle(35, -125, 24, -165, 10, -135);
+
+      // Head
+      p.fill(20, furSaturation, furLightness);
+      p.strokeWeight(0.5);
+      p.stroke(0);
+      p.circle(0, -100, 50);
+
+      if (t < this.blinkTimes[index]) {
+        // Eyes
+        p.strokeWeight(0.25);
+        p.fill(eyeColor, 50, 50);
+        p.ellipse(-20, -115, 10, 8);
+        p.ellipse(20, -115, 10, 8);
+
+        // Pupils
+        p.fill(0);
+        p.ellipse(-20, -115, pupilWidth, 7);
+        p.ellipse(20, -115, pupilWidth, 7);
+      } else {
+        p.strokeWeight(1.5);
+        p.stroke(0);
+        p.line(-25, -115, -15, -115);
+        p.line(25, -115, 15, -115);
+        this.blinkTimes[index] = t + Math.random() * 50 + 10;
+      }
+
+      // Whiskers
+      p.strokeWeight(1);
+      p.stroke(whiskerColor);
+      p.line(-30, -82, -90, -78);
+      p.line(-32, -84, -92, -82);
+      p.line(-31, -86, -92, -85);
+      p.line(-29, -80, -89, -74);
+
+      p.line(30, -82, 90, -78);
+      p.line(32, -84, 92, -82);
+      p.line(31, -86, 92, -85);
+      p.line(29, -80, 89, -74);
+
+      // Nose
+      p.strokeWeight(0.5);
+      p.stroke(0);
+      p.fill(0, 35, 50);
+      p.triangle(0, -91, -5, -100, 5, -100);
+
+      // Mouth
+      p.strokeWeight(1.5);
+      p.stroke(0);
+      p.line(0, -91, 0, -80);
+      p.line(-10, -75, 0, -80);
+      p.line(10, -75, 0, -80);
+
+
+      p.pop();
+    },
+  },
 };
